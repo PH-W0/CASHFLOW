08a7663deb17cf95f2a70482a436bd4c82add9b0
PH-W0 <siphikazisithole@gmail.com>
2025-11-20 18:16:16 +0200

    chore: fixed functionality

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,10 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: ["./client/**/*.{ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
